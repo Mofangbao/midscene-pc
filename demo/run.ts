bb0d0001383c9f076ea4707d433086d67512d9d5
mofangbao <mofangbao@gmail.com>
2025-12-05 08:25:25 +0800

    feat:增加远程示例

--- demo/run.ts
+++ demo/run.ts
@@ -10,14 +10,16 @@ import { writeFile } from "./write.file";
 import { simpleDescription } from "./simple";
 import { sendChatMsg } from "./send.chat.msg";
 import { playMusic } from "./play.music";
+import { remoteMonitor } from "./remote.monitor";
 
 const runDemo = async (pcService: IPCService) => {
   console.log(`running pc agent demo with ${pcService.name}`);
   //   await sendChatMsg(pcService);
   //   await browserUse(pcService);
-  await writeFile(pcService);
+  // await writeFile(pcService);
   // await simpleDescription(pcService);
   // await playMusic(pcService);
+  await remoteMonitor();
 };
 
 (async () => {
