f210f4e77e3bf337ff301881224f346ddaa4ae4f
mofangbao <mofangbao@gmail.com>
2026-01-27 16:19:47 +0800

    feat:restore aiOutput method

--- src/pc.agent.ts
+++ src/pc.agent.ts
@@ -13,12 +13,40 @@ export class PCAgent extends Agent<PCDevice> {
             throw new Error("PCDevice not launched, please call launch() first");
         }
         super(device, agentOptions);
-        const cacheId =
-            agentOptions?.cacheId ?? (agentOptions?.cache as CacheConfig | undefined)?.id;
+        const cacheId = agentOptions?.cacheId ?? (agentOptions?.cache as CacheConfig | undefined)?.id;
         if (cacheId) {
             console.info("pc agent cache enabled,cache id:", cacheId);
             this.storage = new LocalStorage(`./cache/${cacheId}`);
         }
+    }
+
+    private random32UUID() {
+        // 生成一个由字母组成的32位随机数
+        const randomChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let randomStr = "";
+        for (let i = 0; i < 32; i++) {
+            const randomIndex = Math.floor(Math.random() * randomChars.length);
+            randomStr += randomChars.charAt(randomIndex);
+        }
+        return randomStr;
+    }
+
+    public async aiOutput(task: string) {
+        return new Promise((res, rej) => {
+            try {
+                const uid = this.random32UUID();
+                this.device.listenOutput(uid, (output) => {
+                    res(output);
+                });
+                const action = `${task}
+    ==========以下是本次任务的uuid，与任务内容无关，但在调用工具需要传入uuid的时候务必原样传入===========
+    ${uid}
+    `;
+                this.aiAction(action);
+            } catch (error) {
+                rej(error);
+            }
+        });
     }
 
     protected storage: LocalStorage | undefined;
